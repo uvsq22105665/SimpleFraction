21c32cd344790abcc204b769fcc91c136468595f
uvsq22105665 <fanoaraz@gmail.com>
2021-09-30 13:28:24 +0200

    git ignore edit

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,6 +1,21 @@
 public class Fraction {
 
+    int nom;
+    int deno;
+    public Fraction(int a, int b){
+        this.nom = a;
+        this.deno = b;
+    }
+
+    @Override
+    public String toString(){
+        return this.nom + "/" + this.deno;
+    }
+
     public static void main(String[] args) {
-        System.out.println("test");
+        //System.out.println("test");
+        Fraction f = new Fraction(1,2);
+        String fraction = f.toString();
+        System.out.println(fraction);
     }
 }
